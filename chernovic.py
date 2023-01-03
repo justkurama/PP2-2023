aabae8aec39518132bc844101c7a79573b8475b7
justkurama <tolebayevk0723@gmail.com>
2023-01-04 02:50:26 +0600

    lesson 2

---
+++ chernovic.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 450))
+pygame.display.set_caption("MY GAME")
+icon = pygame.image.load('PP2 project/Images/icon.png')
+pygame.display.set_icon(icon)
+run = True
+while run :
+    screen.fill((237, 180, 24))
+    pygame.display.update()
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+            run = False
+        elif i.type == pygame.KEYDOWN:   
+            if i.key == pygame.K_r:
+                screen.fill((74, 255, 77))
+
+    
