729c374a8f01c70ce2d7594a0b3509d6c5a47a19
justkurama <tolebayevk0723@gmail.com>
2023-01-05 02:31:33 +0600

    player

--- Starfall/theGame.py
+++ Starfall/theGame.py
@@ -1,11 +1,16 @@
-import pygame as pg
-import sys
+import pygame as pg, sys
 pg.init()
-screen = pg.display.set_mode((800, 600))
+
+screen = pg.display.set_mode((800, 458))
 pg.display.set_caption("Star fall")
-bg = pg.image.load('Starfall/images/bg.png')
+
+bg = pg.image.load('Starfall/images/bg.jpg')
+player = pg.image.load('Starfall/images/player/player_front.png')
+
 while True:
     screen.blit(bg, (0, 0))
+    screen.blit(player, (380, 300))
+
     pg.display.update()
     for i in pg.event.get():
         if i.type == pg.QUIT:
