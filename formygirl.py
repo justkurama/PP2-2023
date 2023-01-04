ee6ffd755156a370538fd0f9bd660cd8af552741
justkurama <tolebayevk0723@gmail.com>
2023-01-04 23:56:22 +0600

    ok

--- formygirl.py
+++ formygirl.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from turtle import *
 color("red")
 begin_fill()
@@ -10,17 +9,4 @@ right(140)
 circle(50,200)
 forward(133)
 end_fill()
-=======
-from turtle import *
-color("red")
-begin_fill()
-pensize(7)
-left(50)
-forward(133)
-circle(50,200)
-right(140)
-circle(50,200)
-forward(133)
-end_fill()
->>>>>>> b923bf345797930ce41115d08a33d4f7c896c54e
 done()
