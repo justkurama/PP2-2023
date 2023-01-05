243aaf3d074acfc6e94f30a64f10f54d1101b3db
justkurama <tolebayevk0723@gmail.com>
2023-01-05 17:58:32 +0600

    backgroung changes

--- Starfall/theGame.py
+++ Starfall/theGame.py
@@ -1,7 +1,7 @@
 import pygame as pg, sys
 pg.init()
 
-screen = pg.display.set_mode((800, 458))
+screen = pg.display.set_mode((750, 672))
 pg.display.set_caption("Star fall")
 
 bg = pg.image.load('Starfall/images/bg.jpg')
@@ -9,7 +9,7 @@ player = pg.image.load('Starfall/images/player/player_front.png')
 
 while True:
     screen.blit(bg, (0, 0))
-    screen.blit(player, (380, 300))
+    screen.blit(player, (350, 590))
 
     pg.display.update()
     for i in pg.event.get():
