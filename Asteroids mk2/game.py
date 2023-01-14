bd4dfc7588d60c3e92b98ff7d5f11886ca00c451
justkurama <tolebayevk0723@gmail.com>
2023-01-15 00:05:19 +0600

    ok

--- Asteroids mk2/game.py
+++ Asteroids mk2/game.py
@@ -1,3 +1,35 @@
 import pygame as pg
 pg.init()
 
+screenh = 728
+screenw = 409
+screen = pg.display.set_mode((screenw, screenh))
+clock = pg.time.Clock()
+
+icon = pg.image.load('Asteroids mk2/asteroidsPics/asteroid100.png')
+pg.display.set_icon(icon)
+pg.display.set_caption('Asteroids')
+
+bg = pg.image.load('Asteroids mk2 mk2/asteroidsPics/spacebg.png')
+alienImg = pg.image.load('Asteroids mk2/asteroidsPics/alienShip.png')
+playerRocket = pg.image.load('Asteroids mk2/asteroidsPics/spaceRocket.png')
+star = pg.image.load('Asteroids mk2/asteroidsPics/star.png')
+asteroid50 = pg.image.load('Asteroids mk2/asteroidsPics/asteroid50.png')
+asteroid100 = pg.image.load('Asteroids mk2/asteroidsPics/asteroid100.png')
+asteroid150 = pg.image.load('Asteroids mk2/asteroidsPics/asteroid150.png')
+
+shoot = pg.mixer.Sound('Asteroids mk2/sounds/shoot.wav')
+bangLargeSound = pg.mixer.Sound('Asteroids mk2/sounds/bangLarge.wav')
+bangSmallSound = pg.mixer.Sound('Asteroids mk2/sounds/bangSmall.wav')
+shoot.set_volume(.25) 
+bangLargeSound.set_volume(.25)
+bangSmallSound.set_volume(.25)
+
+HP = 5
+run = True
+
+while run:
+    clock.tick(60)
+
+
+
