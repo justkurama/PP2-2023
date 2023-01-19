957a5437b05388abbfa574faef4c57547e04aafc
justkurama <tolebayevk0723@gmail.com>
2023-01-19 13:26:25 +0600

    TSIS1 finished

---
+++ TSIS/TSIS1/PyBool_Ex.py
@@ -0,0 +1,10 @@
+#ex1
+print(10 > 9) #Output: True
+#ex2
+print(10 == 9) #Output: False
+#ex3
+print(10 < 9) #Output: False
+#ex4
+print(bool("abc")) #Output: True
+#ex5
+print(bool(0)) #Output: False
