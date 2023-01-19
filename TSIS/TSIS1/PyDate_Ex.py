1dfdc792de99f84f692e2f5191f2dde4f7cce813
justkurama <tolebayevk0723@gmail.com>
2023-01-19 13:07:36 +0600

    TSIS1 committed

---
+++ TSIS/TSIS1/PyDate_Ex.py
@@ -0,0 +1,21 @@
+#ex1
+x = 5
+print(type(x)) # output: int
+#ex2
+x = "Hello World"
+print(type(x)) # outout: str
+#ex3
+x = 20.5
+print(type(x)) # output: float
+#ex4
+x = ["apple", "banana", "cherry"]
+print(type(x)) # output: list
+#ex5
+x = ("apple", "banana", "cherry")
+print(type(x)) # output: tuple
+#ex6 
+x = {"name" : "John", "age" : 36}
+print(type(x)) #output: dict
+#ex7
+x = True
+print(type(x)) #output: bool
