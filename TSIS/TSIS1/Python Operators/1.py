9bb86c36ce34f9405b583aa1cadc56656adc2f53
justkurama <tolebayevk0723@gmail.com>
2023-01-26 15:10:39 +0600

    TSIS1 completed

---
+++ TSIS/TSIS1/Python Operators/1.py
@@ -0,0 +1,7 @@
+print(10 + 5) #Addition
+print(10 - 5) #Subtraction
+print(10 * 5) #Multiplication
+print(10 / 5) #Division
+print(10 // 5) #Floor division	
+print(10 ** 5) #Exponentiation
+print(10 % 5) #Modulus
---
+++ TSIS/TSIS1/Python Strings/Escape Characters/1.py
@@ -0,0 +1,3 @@
+#The escape character allows you to use double quotes when you normally would not be allowed:
+#To fix this problem, use the escape character \":
+txt = "We are the so-called \"Vikings\" from the north."
